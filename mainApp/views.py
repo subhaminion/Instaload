1c700593a4df1ba9acec60dc06e62108f4b9756a
subham <subham.bhattacharjee1@gmail.com>
2017-08-26 00:15:58 +0530

    The final commit after dinner is like desert for me.

--- mainApp/views.py
+++ mainApp/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.template import loader
 from bs4 import BeautifulSoup
+import requests
 # Create your views here.
 
 
@@ -12,5 +13,12 @@ def index(request):
 
 def download(request):
 	if request.method=='POST':
-		url=request.POST.get('iurl')
-		return HttpResponse(url)
+		url   = request.POST.get('iurl')
+		raw   = requests.get(url)
+		soup  = BeautifulSoup(raw.text, 'html.parser')
+		
+		links = soup.find(property="og:image")
+		image = links.get('content')
+		while image != '':
+			html = '<img src="'+image+'" align="center">'
+			return HttpResponse(html)
