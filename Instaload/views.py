60ccfc912930e2c5db8b22621cd5f2a7eec31679
subham <subham.bhattacharjee1@gmail.com>
2017-08-26 02:34:48 +0530

    I swear this will be final one

--- Instaload/views.py
+++ Instaload/views.py
@@ -4,8 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the main index.")
+    return HttpResponseRedirect("insta/")
--- Instaload/wsgi.py
+++ Instaload/wsgi.py
@@ -11,10 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-path = "home/subhambhattacharjee/Instaload"
-if path not in sys.path:
-	sys.path.append(path)
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Instaload.settings")
 
 application = get_wsgi_application()
