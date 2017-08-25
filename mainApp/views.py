5d53fe377c2b754e0780a89ada8c4871d31d9584
Subham Bhattacharjee <subham.bhattacharjee1@gmail.com>
2017-08-25 22:15:52 +0530

    lol

--- mainApp/views.py
+++ mainApp/views.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.template import loader
 from bs4 import BeautifulSoup
-
 # Create your views here.
 
 
@@ -13,14 +12,5 @@ def index(request):
 
 def download(request):
 	if request.method=='POST':
-		url=request.form('iurl', '')
-		# raw=requests.get(url) #make a request to the URL
-		# soup=BeautifulSoup(raw.text,'html.parser') #get the HTML
-
-		# links= soup.find(property="og:image") #find meta with property=og:image
-		# image=links.get('content') #get its content
-		return HttpResponse('kaam ho raha hai')
-	# while image!='':
-	# 	return '<img src="'+image+'"'+ 'align="center">' #insert content in img tag
-
-	# return render_template('index.html')
+		url=request.POST.get('iurl')
+		return HttpResponse(url)
