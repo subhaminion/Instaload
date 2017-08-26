352e45c3451aa4eb7fc783736aa2225a0700c229
Subham Bhattacharjee <subham.bhattacharjee1@gmail.com>
2017-08-26 12:50:37 +0530

    deleted

--- mainApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='instaurl'),
-    url(r'^insta/download/', views.download, name="download")
-    # url(r'^(?P<album_id>[0-9]+)/$',views.details, name='')
-]
